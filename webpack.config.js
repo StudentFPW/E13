c377516e0a36db7cfb3433aedaf72d86a4ad4373
Leonids Jofe <jofeleonids00@gmail.com>
2023-09-10 17:02:18 +0300

    Keep calm and commit [10/09/2023]

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
+// Choosing modes
+let mode = 'development'
+let HMR = true
+
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+    HMR = false
+}
+
+console.log(mode + ' : mode')
+console.log(HMR + ' : HMR')
+// Choosing modes end
+
 module.exports = {
-    mode: 'development',
+    mode: mode,
     entry: './src/index.js',
     stats: "errors-only",
     devtool: 'inline-source-map',
@@ -13,14 +26,19 @@ module.exports = {
 
     devServer: {
         static: './dist',
-        hot: true,
+        hot: HMR,
         port: 3001,
     },
 
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Практическая работа 13.7 (HW-03)',
+            template: './src/index.html',
         }),
         new ESLintPlugin(),
     ],
+
+    performance: {
+        hints: false
+    }
 }
